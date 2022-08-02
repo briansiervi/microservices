c68d2179d818953930078212a049d5ce1ac44667
Brian Mazini Siervi <brianmazini@gmail.com>
2022-08-02 00:25:45 -0300

    findAll

--- 02-calculator/src/main/java/br/com/brian/controllers/PersonController.java
+++ 02-calculator/src/main/java/br/com/brian/controllers/PersonController.java
@@ -1,6 +1,5 @@
 package br.com.brian.controllers;
 
-import br.com.brian.math.Operation;
 import br.com.brian.model.Person;
 import br.com.brian.services.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,14 +9,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/person")
 public class PersonController {
     @Autowired
     private PersonService service;
 
+    @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Person> findAll() {
+        return service.findAll();
+    }
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public Person findById(@PathVariable(value = "id") String id) {
-       return service.findById(id);
+        return service.findById(id);
     }
 }
--- 02-calculator/src/main/java/br/com/brian/services/PersonService.java
+++ 02-calculator/src/main/java/br/com/brian/services/PersonService.java
@@ -3,13 +3,15 @@ package br.com.brian.services;
 import br.com.brian.model.Person;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Service
 public class PersonService {
     private final AtomicLong counter = new AtomicLong();
 
-    public Person findById(String id){
+    public Person findById(String id) {
         Person person = new Person();
         person.setId(counter.incrementAndGet());
         person.setFirstName("Brian");
@@ -17,6 +19,26 @@ public class PersonService {
         person.setAdress("São Paulo - SP - Brasil");
         person.setGender("Male");
 
+        return person;
+    }
+
+    public List<Person> findAll() {
+        List<Person> persons = new ArrayList<Person>();
+
+        for (int i = 0; i < 8; i++) {
+            Person person = mockPerson(i);
+            persons.add(person);
+        }
+        return persons;
+    }
+
+    private Person mockPerson(int i) {
+        Person person = new Person();
+        person.setId(counter.incrementAndGet());
+        person.setFirstName("Person name " + i);
+        person.setLastName("Last name " + i);
+        person.setAdress("Some address in Brazil " + i);
+        person.setGender("Male");
         return person;
     }
 }
